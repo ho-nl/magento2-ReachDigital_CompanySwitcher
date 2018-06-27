60e46b85caa070a09c38d2375b5788e3d623638a
Jeroen van Leusden <jeroen@reachdigital.nl>
2018-06-27 11:59:06 +0200

    Format before plugin

--- src/Plugin/ModelShippingInformationManagementPlugin.php
+++ src/Plugin/ModelShippingInformationManagementPlugin.php
@@ -3,11 +3,13 @@
  * Copyright © Reach Digital (https://www.reachdigital.io/)
  * See LICENSE.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Ho\CompanySwitcher\Plugin;
 
 use Magento\Checkout\Api\Data\ShippingInformationInterface;
 use Magento\Checkout\Model\ShippingInformationManagement;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Model\QuoteRepository;
 
 class ModelShippingInformationManagementPlugin
@@ -23,15 +25,25 @@ class ModelShippingInformationManagementPlugin
         $this->quoteRepository = $quoteRepository;
     }
 
+    /**
+     * @param ShippingInformationManagement $subject
+     * @param                               $cartId
+     * @param ShippingInformationInterface  $addressInformation
+     *
+     * @throws NoSuchEntityException
+     *
+     * @return array
+     */
     public function beforeSaveAddressInformation(
         ShippingInformationManagement $subject,
         $cartId,
         ShippingInformationInterface $addressInformation
-    ) {
+    ): array {
         $quote = $this->quoteRepository->getActive($cartId);
-
         $shippingAddress = $addressInformation->getShippingAddress();
 
         $quote->setData('own_reference', $shippingAddress->getExtensionAttributes()->getOwnReference());
+
+        return [$cartId, $addressInformation];
     }
 }
